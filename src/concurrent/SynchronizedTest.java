34db9b730a0d1d7f71598ac9b5f67bb5030bc460
yaoyl753951@gmail.com <Da1Bu2Kai3>
2021-03-09 02:18:34 +0800

    synchronized方法编译测试
    Signed-off-by: yaoyl753951@gmail.com <Da1Bu2Kai3>

--- src/concurrent/SynchronizedTest.java
+++ src/concurrent/SynchronizedTest.java
@@ -1,10 +1,18 @@
 package concurrent;
 
 public class SynchronizedTest {
+    static private synchronized void test() {
+        throw new IllegalMonitorStateException();
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        test();
+        int a = 1;
         Object lock = new Object();
         Runnable runnable = () -> {
+            System.out.println(a);
             synchronized (lock) {
+                int b;
                 try {
                     System.out.println("Time stamp: " + System.currentTimeMillis()
                             + " " + Thread.currentThread().getName());
